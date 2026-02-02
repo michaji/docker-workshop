e50b78446c3887b6e6087211731d8117d4df4cc6
Ajiboye Michael <michaelajiboye8@gmail.com>
2026-02-02 18:59:24 +0000

    Refactor Dockerfile and ingest_data.py; add docker-compose.yaml and zones.ipynb for data ingestion

--- pipeline/ingest_data.py
+++ pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm
+import click
 
 
 dtype = {
@@ -30,32 +31,21 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
+def run(
+    pg_user: str = "root",
+    pg_pass: str = "root",
+    pg_host: str = "localhost",
+    pg_port: str = "5432",
+    pg_db: str = "ny_taxi",
+    year: int = 2021,
+    month: int = 1,
+    target_table: str = "yellow_taxi_data",
+    chunksize: int = 100000,
+):
+    prefix = "https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
+    url = f"{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz"
 
-
-
-
-
-
-
-def run():
-    pg_user = 'root'
-    pg_pass = 'root'
-    pg_host = 'localhost'
-    pg_port = '5432'
-    pg_db = 'ny_taxi'
-
-
-    year = 2021
-    month = 1
-
-    target_table = 'yellow_taxi_data'
-
-    chunksize = 100000
-
-    prefix = 'https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-    url = f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
-    
-    engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    engine = create_engine(f"postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}")
 
     df_iter = pd.read_csv(
         url,
@@ -69,19 +59,56 @@ def run():
     for df_chunk in tqdm(df_iter):
         if first:
             df_chunk.head(0).to_sql(
-                name=target_table, 
-                con=engine, 
-                if_exists='replace'
-                )
+                name=target_table,
+                con=engine,
+                if_exists="replace",
+            )
             first = False
         df_chunk.to_sql(
-            name=target_table, 
-            con=engine, 
-            if_exists='append' 
-            )
-        
-if __name__ == '__main__':
-    run()
+            name=target_table,
+            con=engine,
+            if_exists="append",
+        )
+
+
+@click.command()
+@click.option("--pg-user", default="root", show_default=True, help="Postgres username")
+@click.option("--pg-pass", default="root", show_default=False, help="Postgres password")
+@click.option("--pg-host", default="localhost", show_default=True, help="Postgres host")
+@click.option("--pg-port", default="5432", show_default=True, help="Postgres port")
+@click.option("--pg-db", default="ny_taxi", show_default=True, help="Postgres database name")
+@click.option("--year", default=2021, type=int, show_default=True, help="Data year")
+@click.option("--month", default=1, type=int, show_default=True, help="Data month (1-12)")
+@click.option("--target-table", default="yellow_taxi_data", show_default=True, help="Target table name")
+@click.option("--chunksize", default=100000, type=int, show_default=True, help="CSV read chunksize")
+def main(
+    pg_user,
+    pg_pass,
+    pg_host,
+    pg_port,
+    pg_db,
+    year,
+    month,
+    target_table,
+    chunksize,
+):
+    """Ingest NYC taxi data into Postgres."""
+    click.echo(f"Ingesting {year}-{month:02d} into table '{target_table}' on {pg_host}/{pg_db} ...")
+    run(
+        pg_user=pg_user,
+        pg_pass=pg_pass,
+        pg_host=pg_host,
+        pg_port=pg_port,
+        pg_db=pg_db,
+        year=year,
+        month=month,
+        target_table=target_table,
+        chunksize=chunksize,
+    )
+
+
+if __name__ == "__main__":
+    main()
 
 
 
